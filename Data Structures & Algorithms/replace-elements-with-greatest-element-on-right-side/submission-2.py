64125b758940a3c0f63fac06fa58f6bbbbe5ba2e
Kaike de Morais <kaike.carvalho@outlook.com>
2026-09-01 00:19:20 -0300

    submissão repetida

--- Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-2.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def replaceElements(self, arr: List[int]) -> List[int]:
-        rightValue = -1
-        for i in range(len(arr)-1, -1, -1):
-            maxValue = max(rightValue, arr[i])
-            arr[i] = rightValue
-            rightValue = maxValue
-        return arr;
-        
