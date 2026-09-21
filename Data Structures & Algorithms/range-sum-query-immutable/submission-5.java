bcf8ff7307f957186aa42a8ba5e1abb7a33b7c4d
Kaike de Morais <kaike.carvalho@outlook.com>
2026-09-21 19:43:31 -0300

    Add: range-sum-query-immutable - submission-5

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-5.java
@@ -0,0 +1,24 @@
+class NumArray {
+
+    private int[] arr;
+
+    public NumArray(int[] nums) {
+        this.arr = new int[nums.length];
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            this.arr[i] = sum;
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        if(left == 0){return arr[right];}
+        return arr[right] - arr[left-1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
